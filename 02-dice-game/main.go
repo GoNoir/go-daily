b3bd48046b049ea2450429894bcfbc6f9a2350ca
GoNoir <weisbrodt.stefan+1@gmail.com>
2025-10-08 23:19:07 +0100

    Add game structure and DiceRoll function

--- 02-dice-game/main.go
+++ 02-dice-game/main.go
@@ -14,11 +14,18 @@ package main
 import (
 	"fmt"
 	"math/rand"
+	//"bufio"
+	//"os"
 )
 
 //create dice, for now - slice (struct or enums could be better - why?)
 
 //roll dice - pseudorandomness from rand library. Good enough for this, but not secure enough for security purpose.
+
+func DiceRoll (s []int) int {
+return rand.Intn(len(s))
+}
+
 //ask user for the next one - need another library - bufio?
 
 //roll dice again
@@ -31,17 +38,28 @@ import (
 func main() {
 //keep score
 score := 0
+//loop 5 times
+
+//for i := 1; i < 5; i++ {
 //dice is a slice
 dice := []int{1, 2, 3, 4, 5, 6}
-//roll - we need to extrapolate roll later for DRY - needs to be checked agains the predition, like lastRoll := DiceRoll(dice)
-fmt.Println(dice[rand.Intn(len(dice))])
-//Ask user to give higher or lower prediction eg. h or l 
+x := DiceRoll(dice)
+fmt.Println(x)
+//ask user for prediction: need bufio/scanner for command line input
+//fmt.Printf("The last score was: %d", x)
+//fmt.Printf("Will next roll be higher or lower?"
+//takes h or l user input
+//y := DiceRoll(dice)
+switch
+case h
+//if //user input h and y > x {
+score++
+fmt.Println("You won!")
+//else {
+fmt.Println("You lose :("
+case l
+//
 
-//Loop 4 more times
-//for i := 1; i < 5; i++ {
-//roll dice again
 //update score and print for user
-
-
 //print final score
 }
