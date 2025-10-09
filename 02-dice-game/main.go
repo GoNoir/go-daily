0689d9c611bb7919c5e666f4a58de11635500773
GoNoir <weisbrodt.stefan+1@gmail.com>
2025-10-09 23:36:12 +0100

    Version 1 - added working bufio scanner, switch and input validation

--- 02-dice-game/main.go
+++ 02-dice-game/main.go
@@ -1,9 +1,9 @@
 /*
 Dice Game
-Focus: Random numbers, loops, conditionals
+Focus: Random numbers, for loop, switch and if, user input in cli: bufio/scan
 What it does: Simple "guess if next roll is higher/lower" game
 
-Roll a die (1-6), show number (create a dice - enums?) 
+Roll a die (1-6), show number
 Ask user: will next roll be higher/lower?
 Roll again, tell them if they won
 Keep score, play 5 rounds
@@ -12,54 +12,53 @@ Keep score, play 5 rounds
 package main
 
 import (
+	"bufio"
 	"fmt"
 	"math/rand"
-	//"bufio"
-	//"os"
+	"os"
 )
 
-//create dice, for now - slice (struct or enums could be better - why?)
-
-//roll dice - pseudorandomness from rand library. Good enough for this, but not secure enough for security purpose.
-
-func DiceRoll (s []int) int {
-return rand.Intn(len(s))
+func DiceRoll(s []int) int {
+	return s[rand.Intn(len(s))]
 }
 
-//ask user for the next one - need another library - bufio?
-
-//roll dice again
-//print won/lose
-
-//add to score
-
-//repeat 5 times.
-
 func main() {
-//keep score
-score := 0
-//loop 5 times
+	score := 0
+	for i := 1; i < 6; i++ {
+		dice := []int{1, 2, 3, 4, 5, 6}
+		x := DiceRoll(dice)
+		fmt.Printf("First roll was: %d:", x)
+		scanner := bufio.NewScanner(os.Stdin)
+		fmt.Printf("Will next roll be [h]igher or [l]ower?:")
+		scanner.Scan()
+		input := scanner.Text()
+		if input != "h" && input != "l" {
+			fmt.Printf("Sorry, can only accept 'h' for higher or 'l' for lower:")
+			scanner.Scan()
+			input = scanner.Text()
+		}
+		fmt.Printf("Yout typed: %q, let's roll the dice...", input)
 
-//for i := 1; i < 5; i++ {
-//dice is a slice
-dice := []int{1, 2, 3, 4, 5, 6}
-x := DiceRoll(dice)
-fmt.Println(x)
-//ask user for prediction: need bufio/scanner for command line input
-//fmt.Printf("The last score was: %d", x)
-//fmt.Printf("Will next roll be higher or lower?"
-//takes h or l user input
-//y := DiceRoll(dice)
-switch
-case h
-//if //user input h and y > x {
-score++
-fmt.Println("You won!")
-//else {
-fmt.Println("You lose :("
-case l
-//
+		y := DiceRoll(dice)
 
-//update score and print for user
-//print final score
+		switch input {
+		case "h":
+			if y > x {
+				score++
+				fmt.Println("You won!")
+			} else {
+				fmt.Println("You lose :/")
+			}
+		case "l":
+			if y < x {
+				score++
+				fmt.Println("You won!")
+			} else {
+				fmt.Println("You lose :/")
+			}
+
+		}
+
+	}
+	fmt.Printf("Your final score is: %d", score)
 }
