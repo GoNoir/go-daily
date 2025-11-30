89387473fd2ac513183ca12b849e85d4cc7c4608
GoNoir <weisbrodt.stefan+1@gmail.com>
2025-11-30 19:02:33 +0000

    Reading from os.File types, including os.Stin if no file is given.

---
+++ 07-multi-files-cli/main.go
@@ -0,0 +1,39 @@
+//Multi-files-cli program reads from a list of files and from cli and reads every line which occur more than once - returns ths line and its count
+package main
+
+import (
+	"os"
+	"fmt"
+	"bufio"
+)
+
+func main() {
+	counts := make(map[string]int)
+	files := os.Args[1:]
+	if len(files) == 0 {
+		countLines(os.Stdin, counts)
+	} else {
+		for _, arg := range files {
+			f, err := os.Open(arg)
+			if err != nil {
+				fmt.Fprintf(os.Stderr, "Dup: %v\n", err)
+				continue
+			}
+			countLines(f, counts) 
+			f.Close()
+		}
+	}
+	for line, n := range counts {
+		if n > 1 {
+			fmt.Printf("%d\t%s\n", n, line)
+		}
+	}
+}
+
+func countLines(f *os.File, counts map[string]int) {
+	input := bufio.NewScanner(f)
+	for input.Scan() {
+		counts[input.Text()]++
+	}
+}
+
